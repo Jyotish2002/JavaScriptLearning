21a720b126f06aab526224f18045bcb7b40526e3
Jyotish <jyotishyadav09@gmail.com>
2025-06-23 23:31:39 +0530

    Day_03 Operation

---
+++ basic_01/03_Operation.js
@@ -0,0 +1,38 @@
+// ********************** Operation **********************
+
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3)
+// console.log(2%3);
+
+// let str1 = "Hello";
+// let str2 = " Jyotish";
+// let str3 = str1 + str2;
+// // let str3= str1.concat(str2);
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(+true); //1
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+
+// let gameCounter = 100;
+// game=gameCounter++;
+// console.log(gameCounter); // 101
+// console.log(game);// 100
+
+let a=20;
+b=a++; // postfix
+console.log("The value of a is 20 but not it is = " +a); //21
+console.log("The valeu of b is = "+b); //20
---
+++ basic_01/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+
+// let gameCounter = 100;
+// game=gameCounter++;
+// console.log(gameCounter); // 101
+// console.log(game);// 100
