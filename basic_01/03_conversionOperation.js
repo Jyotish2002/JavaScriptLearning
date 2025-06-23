662fd2bb323892980159b9fe5771ff07d55160cd
Jyotish <jyotishyadav09@gmail.com>
2025-06-23 23:14:51 +0530

    conversion of datatypes

---
+++ basic_01/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score ="Jyotish";
+
+console.log(typeof score); // string
+
+let valueInNumber = Number(score); // change string to number
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber);
+//  "33" => 33
+//  "33abc" => NaN
+//  true => 1 ; false = > 0
+
+ let isLoggedIn =1;
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(typeof booleanIsLoggedIn); // boolean
+ console.log(booleanIsLoggedIn); // true
+
+//  1 = true; 0 = false;
+// ""=>false 
+// "name" = > true 
+
+let someNumber = 33
+let stringNumber= String(someNumber);
+console.log(typeof stringNumber); // string
+ 
