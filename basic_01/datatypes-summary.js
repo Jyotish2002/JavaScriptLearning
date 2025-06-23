9eeaea49ef8db0fc547afa8aac129c9a15330b1e
Jyotish <jyotishyadav09@gmail.com>
2025-06-23 23:53:03 +0530

    datatypes Summary

---
+++ basic_01/datatypes-summary.js
@@ -0,0 +1,32 @@
+// # Primitive DataType
+
+// 7 types : String, Number, boolean, Null, undefined,Symbol,BigInt
+
+const score= 100;
+const scoreValue=100.3
+const isLoggedIn =false;
+const outsideTemp=null;
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId); //false
+const bigNumber= 3454154545678785646545645n
+
+// reference (Non primitive)
+ //Array, Objects, Functions
+
+const heros =["shaktiman","naagraj","doga"];//array
+let obj= {
+    name:"Jyotish",
+    age:21,
+
+} // object
+console.log(obj.name);//Jyotish
+const myFunction= function(){
+    console.log("Hello World this is function");
+} // function
+myFunction();
+
+console.log(typeof anotherId);
