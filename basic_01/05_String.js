746672a392cbb4a13b49666594f25f7b0ced7170
Jyotish <jyotishyadav09@gmail.com>
2025-06-24 11:43:15 +0530

    String

---
+++ basic_01/05_String.js
@@ -0,0 +1,38 @@
+const name="Jyotish"
+const repo=50
+
+// console.log(name+repo+" values")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repo}`); //Modern way to represent string
+
+const gameName= new String("Rocky-Bhaiya")
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('B'));
+
+// const newString=gameName.substring(0,6); // take upto Rocky-
+// console.log(newString);
+
+// const anotherString=gameName.slice(-8, 5); //y
+// console.log(anotherString);
+
+
+// const newStringOne = " Jyotish   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());// It will remove the extra space
+
+const url="https://jyotishyadav.com/home%20page"
+
+console.log(url.replace('%20','-'))
+console.log(url.includes('jyotish'));
+let name1="Jyotish-Yadav-Naihati";
+console.log(name1.split('-'));
+
+
+
+
+
+
