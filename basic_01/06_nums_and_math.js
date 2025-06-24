680b6ac7298cc9494619ebeeb26eb08772096fa8
Jyotish <jyotishyadav09@gmail.com>
2025-06-24 12:07:10 +0530

    maths and numbers

---
+++ basic_01/06_nums_and_math.js
@@ -0,0 +1,41 @@
+// const score=400;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);//3
+// console.log(typeof balance.toString());// string
+// console.log(balance.toFixed(3))//100.000
+
+// const otherNumber=23.8966;
+// console.log(otherNumber.toPrecision(3));// 23.9
+
+// const hundreds=100000
+// console.log(hundreds.toLocaleString('en-IN')); //1,00,00 basically it give comma to understand the currency
+
+// +++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //4
+// console.log(Math.round(4.8)) //5
+// console.log(Math.ceil(4.2)) //5 akways higher values
+// console.log(Math.floor(4.9))//4 always lower values
+// console.log(Math.max(23,12,45,63,11,32));
+// console.log(Math.min(12,10,5,11,2));
+
+console.log(Math.round(Math.random()*10)); // generate random number
+
+const max=20;
+const min=10;
+console.log(Math.floor(Math.random()*(max-min+1) + min)); // it gives between 10 and 20
+
+
+
+
+
+
+
+
+
