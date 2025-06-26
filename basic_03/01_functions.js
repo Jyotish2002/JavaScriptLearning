ae74a4bd487bb360de555d1cba7f470631496248
Jyotish <jyotishyadav09@gmail.com>
2025-06-27 01:06:35 +0530

    scope

--- basic_03/01_functions.js
+++ basic_03/01_functions.js
@@ -34,4 +34,30 @@ function loginUserMessage(username="Rohan"){
 }
 
 let a=loginUserMessage("Sumit");
-console.log(a);
+// console.log(a);
+
+
+function calculateCartPrice(...num1){
+  return num1;
+}
+
+// console.log(calculateCartPrice(200,400,500,2000)) // basically ...num1 will take all number
+
+const user={
+  username:"Jyotish",
+  price:1999
+}
+
+function handleObject(anyobject){
+  console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+const myNewArray=[200,400,100,600];
+
+function returnSecondValue(getArray){
+  return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
---
+++ basic_03/02_Scope.js
@@ -0,0 +1,51 @@
+let a=300
+if (true) {
+  let a = 10;
+  const b = 30;
+//   console.log(a)
+  
+}
+
+
+// console.log(a);
+// console.log(b);
+
+function one(){
+    const username="Jyotish"
+
+    function two(){
+        const website="Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one();
+
+if(true){
+    const username="Jyotish"
+    if(username==="Jyotish"){
+        const webstie=" Youtube";
+        // console.log(username+webstie)
+    }
+    // console.log(webstie);
+    
+}
+// console.log(username);
+
+
+// +++++++++++++++++++++Interesting ++++++++++++++++++++++
+
+
+function addone(value){
+    return value+1;
+}
+a=addone(5);
+
+const addTwo= function(num){
+    return num +2;
+}
+
+addTwo(8);
