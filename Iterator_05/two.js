863c02db4b82a2ef6ffead6f70a62468899c6fa3
Jyotish <jyotishyadav09@gmail.com>
2025-07-04 03:17:15 +0530

    part two

---
+++ Iterator_05/two.js
@@ -0,0 +1,19 @@
+let index=0;
+
+while(index<=10){
+    console.log(`Value of index is ${index}`);
+    index+=2;
+}
+
+let myarray=['Flash','Batman','Superman'];
+
+let arr=0;
+while(arr<myarray.length){
+    console.log(myarray[arr]);
+    arr++;
+}
+let score=11;
+do {
+    console.log(`score is ${score}`); //kam paheli then checking in while
+    score++;
+} while (score<=10);
