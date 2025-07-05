593a724295453b369e7e0e28c10654082d4b9040
Jyotish <jyotishyadav09@gmail.com>
2025-07-05 16:23:17 +0530

    Color Changing

---
+++ 07_Projects/project_05/app.js
@@ -0,0 +1,35 @@
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+
+
+let intervalId;
+const startChangeColor = function () {
+    if(!intervalId){ //best practice
+        intervalId=setInterval(changeBgColor,1000); 
+ 
+    }
+   
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+
+
+
+
+const stopChangeColor = function () {
+    clearInterval(intervalId);
+    intervalId=null;
+};
+
+
+document.querySelector("#start").addEventListener("click", startChangeColor);
+
+document.querySelector("#stop").addEventListener("click", stopChangeColor);
